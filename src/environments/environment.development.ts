81281c72520184e2718cef8b72699db94b11f323
AlexRGB <alexismtz200326@gmail.com>
2024-07-31 23:05:22 -0600

    Url de Render

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  API_URL: 'http://127.0.0.1:5000',
+  API_URL: 'https://calmamente-api.onrender.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  API_URL: '',
+  API_URL: 'https://calmamente-api.onrender.com',
 };
